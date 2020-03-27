3ccea49d809ee6fc923461eb66afaf4f8988a2fb
MatheusPetry <petrysff@gmail.com>
2020-03-27 16:33:39 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+
+import React from "react";
+
+// View = div, header, body, etc
+// Text = h1, p, strong, etc
+
+import Routes from "./src/routes";
+
+export default function App() {
+  return <Routes />;
+}
